4e451d68dd8917b836f72d3be1c2783f8a06ee78
pazbarda <pazbarda@gmail.com>
2022-05-03 10:34:00 +0300

    ValidateMove delegate

--- YourMoveApp/commons/plugin/IGamePlugin.cs
+++ YourMoveApp/commons/plugin/IGamePlugin.cs
@@ -1,20 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using YourMoveApp.commons.model;
+﻿using YourMoveApp.commons.model;
 
 namespace YourMoveApp.commons.plugin
 {
     public interface IGamePlugin
     {
-        public GameState CreateGame(String initiatingPlayerId);
+        public GameState CreateGame(string initiatingPlayerId);
 
-        public GameState JoinGame(String joiningPlayerId, GameState gameState);
+        public GameState JoinGame(string joiningPlayerId, GameState gameState);
 
         public GameState ProcessMove(Move move, GameState gameState);
 
         public GenericResponse ValidateMove(Move move, GameState gameState);
+
+        public Func<Move, GameState, GenericResponse> GetMoveValiator() { return ValidateMove; }
     }
 }
